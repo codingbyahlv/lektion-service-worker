52b269201018f304dadbb39c73c141996547f2a2
Ann-Helen Lausmaa <code@ahlvdesign.se>
2022-05-12 11:31:41 +0200

    update comments

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,15 @@
-//installera och aktivera
+//INSTALLERA OCH AKTIVERA SERVICE WORKER
+
+//installera
 self.addEventListener('install', (event) => {
+  //gör att webbläsaren själv uppdaterar SW vid ändringar
   self.skipWaiting();
   //triggas när den installerats
   console.log('Service worker installed!')
 })
 
-self.addEventListener('activatoe', (event) => {
+//aktivera
+self.addEventListener('activate', (event) => {
   //triggas när den aktiverats
   console.log('Service worker activated!')
 })
@@ -14,11 +18,14 @@ self.addEventListener("fetch", (event) => {
   // Skriv ut url:en på varje närverksförfrågan
   console.log(event.request.url); 
   //kontrollera om du är online/offline
+  //om du är online
   if(navigator.onLine){
     console.log('Du är online')
+  //annars = offline
   } else {
     console.log('Du är offline')
     //om vi är offline kan vi skicka ett svar tillbaka
+    //här kan man också länka in ett specialanpassat html-dokument
     event.respondWith(new Response('<h1>I find your lack of internet disturbing </h1>', 
                     {'headers': {'Content-Type': 'text/html'}}
     ))
